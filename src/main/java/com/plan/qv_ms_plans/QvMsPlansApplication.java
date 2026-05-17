63e030a703a17e1e251039ab5cbcb7e130e46cf0
EilynFlorez <florezganane@gmail.com>
2026-05-17 11:01:13 -0500

    feat(config): initial ms-plans microservice setup

---
+++ src/main/java/com/plan/qv_ms_plans/QvMsPlansApplication.java
@@ -0,0 +1,13 @@
+package com.plan.qv_ms_plans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QvMsPlansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QvMsPlansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plan/qv_ms_plans/QvMsPlansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plan.qv_ms_plans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QvMsPlansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
